d457f404a036c2334a8c3885cf40cda2c1fa0c4e
jadeleonard <lukabartolomeo9@gmail.com>
2024-06-13 07:40:49 +0800

    adde

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
-const path = require('path');
+import { resolve as _resolve } from 'path';
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.ts', // Your main entry point
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    library: 'greentoolsdesign', // Replace with your library name
-    umdNamedDefine: true
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
-        }
+export const mode = 'production';
+export const entry = './src/index.ts';
+export const output = {
+  path: _resolve(__dirname, 'dist'),
+  filename: 'index.js',
+  libraryTarget: 'umd',
+  library: 'greentoolsdesign', // Replace with your library name
+  umdNamedDefine: true
+};
+export const resolve = {
+  extensions: ['.ts', '.tsx', '.js', '.jsx']
+};
+export const module = {
+  rules: [
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'ts-loader'
       }
-    ]
-  }
+    }
+  ]
 };
